ec1fad80928455c1a4098e32e5b136d99f5b232b
majunpo <junpo.ma@intel.com>
2024-11-07 11:55:52 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+ 
+import (
+    "fmt"
+    "github.com/sirupsen/logrus"
+)
+ 
+func main() {
+    fmt.Println("Hello, Go Module!")
+    logrus.Info("This is a log message from logrus")
+}
